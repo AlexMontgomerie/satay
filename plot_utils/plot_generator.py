6aea995466e7cb6bf7168ec6525fc0a3faad466c
Petros Toupas <ptoupas@gmail.com>
2023-07-27 01:45:22 +0300

    Add plot generation for the weights quantization tests on yolo models

--- plot_utils/plot_generator.py
+++ plot_utils/plot_generator.py
@@ -1,3 +1,4 @@
+import matplotlib.gridspec as gridspec
 import matplotlib.pyplot as plt
 import pandas as pd
 import scienceplots
@@ -21,10 +22,13 @@ plt.rcParams["figure.figsize"] = (FIG_SIZE_X, FIG_SIZE_Y)
 plot_generation = "quantization_comparison"
 assert plot_generation in ["yolo_comparison", "quantization_comparison", "all"], "plot_generation must be one of 'yolo_comparison', 'quantization_comparison' or 'all'"
 
-# you can choose the version of the plot to generate (available versions are 1 and 2)
+# you can choose the version of the yolo_comparison plot to generate (available versions are 1 and 2)
 yolo_comparison_version = 2
 assert yolo_comparison_version in [1, 2], "yolo_comparison_version must be one of 1 or 2"
 
+# you can choose the version of the quantization_comparison plot to generate (available versions are "combined" and "separate")
+quantization_comparison_version = "separate"
+assert quantization_comparison_version in ["combined", "separate"], "quantization_comparison_version must be one of 'combined' or 'separate'"
 
 if plot_generation in ["yolo_comparison", "all"]:
     cpu_gpu_df = pd.read_csv('Results_CPU_and_GPU.csv')
@@ -96,7 +100,7 @@ if plot_generation in ["yolo_comparison", "all"]:
 
     plt.tight_layout()
     # plt.savefig(f'yolo_comparison_devices_v{yolo_comparison_version}.png', bbox_inches='tight')
-    plt.savefig(f'yolo_comparison_devices.pdf', bbox_inches='tight', format='pdf')
+    plt.savefig('yolo_comparison_devices.pdf', bbox_inches='tight', format='pdf')
 
 if plot_generation in ["quantization_comparison", "all"]:
     quant_df = pd.read_csv('Results_Quantization.csv')
@@ -104,4 +108,74 @@ if plot_generation in ["quantization_comparison", "all"]:
     print(quant_cols)
 
     # filter based on Input Shape
-    cpu_gpu_df = cpu_gpu_df[cpu_gpu_df['Input Shape'].isin(['640x640'])]
+    quant_df = quant_df[quant_df['Input Shape'].isin(['640x640'])]
+
+    # get the Model Families
+    model_families = quant_df['Model Family'].unique().tolist()
+
+    if quantization_comparison_version == "combined":
+
+        FONT_SIZE_TICKS = 15
+        FONT_SIZE_LABELS = 22
+        FONT_SIZE_TITLE = 20
+        FONT_SIZE_LEGEND = 13
+        FONT_SIZE_TEXT = 15
+
+        fig = plt.figure()
+        gs = gridspec.GridSpec(2, 4)
+        errorbar = lambda x: (x.min(), x.max())
+
+        for idx, model_family in enumerate(model_families):
+            curr_df = quant_df[quant_df['Model Family'].isin([model_family])]
+            if idx == 0:
+                curr_ax = fig.add_subplot(gs[0, :2])
+            elif idx == 1:
+                curr_ax = fig.add_subplot(gs[0, 2:])
+            elif idx == 2:
+                curr_ax = fig.add_subplot(gs[1, 1:3])
+            sns.lineplot(data=curr_df, x='Weights Wordlength', y='mAP50-95 @ coco-val2017\nwith LAYER_BFP', ax=curr_ax, hue='Model Variant', palette=COLOUR_PALETTE, errorbar=errorbar, estimator="mean", err_style='band', linewidth=2, markersize=7, marker='o')
+
+            # Make the title bold
+            curr_ax.set_title(model_family, fontsize=FONT_SIZE_TITLE)
+            curr_ax.set_xlabel('Weights Wordlenght', fontsize=FONT_SIZE_LABELS)
+            curr_ax.set_ylabel('mAP50-95', fontsize=FONT_SIZE_LABELS)
+            # curr_ax.set_yscale('log')
+            handles, labels = curr_ax.get_legend_handles_labels()
+            curr_ax.legend(handles, labels, fontsize=FONT_SIZE_LEGEND, frameon=False)
+
+            curr_ax.set_xticklabels(curr_ax.get_xticklabels(), fontsize=FONT_SIZE_TICKS)
+            curr_ax.set_yticklabels(curr_ax.get_yticklabels(), fontsize=FONT_SIZE_TICKS)
+
+        plt.tight_layout()
+        # plt.savefig('quant_comparison_devices_combined.png', bbox_inches='tight')
+        plt.savefig('quant_comparison_devices_combined.pdf', bbox_inches='tight', format='pdf')
+
+    elif quantization_comparison_version == "separate":
+
+        FONT_SIZE_TICKS = 20
+        FONT_SIZE_LABELS = 30
+        FONT_SIZE_TITLE = 30
+        FONT_SIZE_LEGEND = 20
+        FONT_SIZE_TEXT = 15
+
+        for idx, model_family in enumerate(model_families):
+            curr_df = quant_df[quant_df['Model Family'].isin([model_family])]
+
+            fig, ax = plt.subplots()
+
+            # errorbar = 'ci' or 'pi' or 'sd' or 'se'
+            errorbar = lambda x: (x.min(), x.max())
+            sns.lineplot(data=curr_df, x='Weights Wordlength', y='mAP50-95 @ coco-val2017\nwith LAYER_BFP', ax=ax, hue='Model Variant', palette=COLOUR_PALETTE, errorbar=errorbar, estimator="mean", err_style='band', linewidth=2, markersize=7, marker='o')
+
+            ax.set_xlabel('Weights Wordlenght', fontsize=FONT_SIZE_LABELS)
+            ax.set_ylabel('mAP50-95', fontsize=FONT_SIZE_LABELS)
+            # ax.set_yscale('log')
+            handles, labels = ax.get_legend_handles_labels()
+            ax.legend(handles, labels, loc='upper center', bbox_to_anchor=(0.5, 1.13), ncol=4, fontsize=FONT_SIZE_LEGEND, frameon=False)
+
+            ax.set_xticklabels(ax.get_xticklabels(), fontsize=FONT_SIZE_TICKS)
+            ax.set_yticklabels(ax.get_yticklabels(), fontsize=FONT_SIZE_TICKS)
+
+            plt.tight_layout()
+            # plt.savefig(f'quant_comparison_devices_{model_family}.png', bbox_inches='tight')
+            plt.savefig(f'quant_comparison_devices_{model_family}.pdf', bbox_inches='tight', format='pdf')
